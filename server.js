4fef9dc265277a5bce58c56bfac8f0db4f1496fc
Louis DEBRAINE <louisdebraine@gmail.Com>
2017-04-13 10:43:46 +0200

    Update wording

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
+const port = process.env.PORT || 8000;
 
 // $ GET /hello.txt
 app.use(serve('app'));
@@ -8,6 +9,6 @@ app.use(serve('app'));
 // or use absolute paths
 // app.use(serve(__dirname + '/test/fixtures'));
 
-app.listen(8000);
+app.listen(port);
 
-console.log('listening on port 8000');
+console.log('listening on port '+port);
